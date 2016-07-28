dc0576b7a0ca88b926a24311acb113e1dc4e2540
Tammo Heeren <tammo.heeren@gmail.com>
2016-07-28 14:33:20 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+__author__ = "Tammo Heeren"
+__copyright__ = ""
+__credits__ = [""]
+__license__ = ""
+__version__ = "0.0.1"
+__maintainer__ = ""
+__email__ = "tammo@incredibl.us"
+__status__ = "Development"
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    pass
